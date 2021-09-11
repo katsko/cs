5b1e9bf1977f2cc04c33d8464f7ee9c8b4694cd5
Alexandr Katsko <alexandr.katsko@gmail.com>
2021-09-11 23:41:57 +0700

    linked list: doctests

--- linked_list.py
+++ linked_list.py
@@ -4,6 +4,40 @@ class Elem:
         self.link_next = None
 
 class LinkedList:
+    """
+    >>> ll = LinkedList()
+    >>> ll.get_array()  # empty list
+    []
+    >>> ll.insert(0, 'd')  # first element
+    >>> ll.get_array()
+    ['d']
+    >>> ll.insert(0, 'b')  # new first element before 'd'
+    >>> ll.get_array()
+    ['b', 'd']
+    >>> ll.insert(1, 'c')
+    >>> ll.get_array()
+    ['b', 'c', 'd']
+    >>> ll.insert(-10, 'a')  # new first element
+    >>> ll.get_array()
+    ['a', 'b', 'c', 'd']
+    >>> ll.insert(10, 'e')  # last element
+    >>> ll.get_array()
+    ['a', 'b', 'c', 'd', 'e']
+
+    >>> ll = LinkedList()  # new list
+    >>> ll.get_array()
+    []
+    >>> ll.insert(-10, 'a')  # -10, but 'a' is first element in new list
+    >>> ll.get_array()
+    ['a']
+
+    >>> ll = LinkedList()  # new list
+    >>> ll.get_array()
+    []
+    >>> ll.insert(10, 'b')  # 10, but 'b' is first element in new list
+    >>> ll.get_array()
+    ['b']
+    """
     def __init__(self):
         self._linked_list = {}
         self._link_first = None
@@ -37,4 +71,3 @@ class LinkedList:
             result.append(elem.data)
             link = elem.link_next
         return result
-
